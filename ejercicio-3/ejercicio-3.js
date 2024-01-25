5fced8b30446fc173268c4d2e0c9b3223d15a144
Rodolfo Santini <itsmrsantini@gmail.com>
2024-01-24 21:15:13 -0600

    probando ejercicio 3

---
+++ ejercicio-3/ejercicio-3.js
@@ -0,0 +1,10 @@
+// 3. Crear un programa que determine si un número introducido en un Prompt es par o no, la respuesta será mostrada en un Alert.
+
+
+let par = prompt("Dame un numero y te dire si es par");
+if (par % 2 === 0){
+    alert("es par")
+}
+else{
+    alert("no es par")
+}
