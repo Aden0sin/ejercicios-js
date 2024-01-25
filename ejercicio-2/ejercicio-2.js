bae381df7205db9c584bd79bc968f6a0834d14ca
Rodolfo Santini <itsmrsantini@gmail.com>
2024-01-24 21:08:22 -0600

    arreglos

--- ejercicio-2/ejercicio-2.js
+++ ejercicio-2/ejercicio-2.js
@@ -1,11 +1,12 @@
 // 2. Solicitar al usuario un número, y determinar si es divisible entre dos o no. Mostrando al usuario un mensaje de “x número es divisible entre 2” o “x núm no es divisible entre 2”.
 
-let divisible = prompt("Dame un numero"){
+let divisible = prompt("Dame un numero");
+{
     if (divisible % 2 === 0) {
         alert(divisible + "numero es divisible entre 2")
     }
 
     else{
-        alert(divisble + "numero no es divisble entre 2")
+        alert(divisible + "numero no es divisble entre 2")
     }
 } 
