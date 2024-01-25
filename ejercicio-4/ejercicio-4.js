9ecfdf8348c28da3896e6fe6404ed2c877621a28
Rodolfo Santini <itsmrsantini@gmail.com>
2024-01-24 21:33:21 -0600

    sirve

--- ejercicio-4/ejercicio-4.js
+++ ejercicio-4/ejercicio-4.js
@@ -1,7 +1,7 @@
 //4. Solicitar al usuario un número de cliente. Si el número es el 1000, imprimir 'Ganaste un premio', en caso contrario mostrar el número y el mensaje “Lo sentimos, sigue participando”.
 
 let ganador = prompt("Danos tu numero");
-if( ganador === 100){
+if( ganador === "1000"){
     alert("Ganaste un premio")
 }
 else {
