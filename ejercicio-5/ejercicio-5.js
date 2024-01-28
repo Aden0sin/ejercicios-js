22c14c8cb5832fefbdd264bcbcf1327915b76477
Rodolfo Santini <itsmrsantini@gmail.com>
2024-01-28 13:32:33 -0600

    correcion de operacion en la funcion

--- ejercicio-5/ejercicio-5.js
+++ ejercicio-5/ejercicio-5.js
@@ -1,12 +1,12 @@
 //5. Solicitar al usuario que ingrese dos números y mostrar cuál de los dos es menor. No considerar el caso en que ambos números son iguales.
-function ejercicio5(a,b){
+function ejercicio5(){
     let a = prompt("Dame un numero");
     let b = prompt("Dame otro numero");
     if(a < b){
     alert( a + " es menor que " + b )
     }
     else if(b < a){
-        alert(b + " es menor que " + b)
+        alert(b + " es menor que " + a)
     }
     else{
         alert("Por favor introducir dos numeros distintos")
