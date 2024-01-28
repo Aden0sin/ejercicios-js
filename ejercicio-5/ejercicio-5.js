51052328ac52bcc450f98178bf428ac49d62f737
Rodolfo Santini <itsmrsantini@gmail.com>
2024-01-28 13:30:13 -0600

    corrigiendo nombre de funcion

--- ejercicio-5/ejercicio-5.js
+++ ejercicio-5/ejercicio-5.js
@@ -1,5 +1,5 @@
 //5. Solicitar al usuario que ingrese dos números y mostrar cuál de los dos es menor. No considerar el caso en que ambos números son iguales.
-function ejercicio-5(a,b){
+function ejercicio5(a,b){
     let a = prompt("Dame un numero");
     let b = prompt("Dame otro numero");
     if(a < b){
