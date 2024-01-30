bb316d746cf3b3b40da5c0cb92165274a77eb579
Rodolfo Santini <itsmrsantini@gmail.com>
2024-01-29 21:47:17 -0600

    arregle el ejercicio

--- ejercicio-7/ejercicio-7.js
+++ ejercicio-7/ejercicio-7.js
@@ -4,31 +4,33 @@
 function MensajeDelDia(){
     alert("Dime que día de la semana es y te dire algo")
     let dia = prompt("Que día es hoy?")
-    if(dia = "Lunes" || "lunes"){
+
+    dia = dia.toLowerCase().trim()
+    if(dia == "lunes"){
         console.log("Primer día de la semana, tu puedes!")
         alert("Primer día de la semana, tu puedes!")
     }
-    else if(dia = "Martes" || "martes"){
+    else if(dia == "martes"){
         console.log("El día mas dificil")
         alert("El día mas dificil")
     }
-    else if(dia = "Miercoles" || "miercoles"){
+    else if(dia == "miercoles"){
         console.log("Feliz joroba de la semana!")
         alert("Feliz joroba de la semana!")
     }
-    else if(dia = "Jueves" || "jueves"){
+    else if(dia == "jueves"){
         console.log("Feliz Jueves!")
         alert("Feliz Jueves!")
     }
-    else if(dia = "Viernes" || "viernes"){
+    else if(dia ==="viernes"){
         console.log("A descansar!")
         alert("A descansar!")
     }
-    else if(dia = "Sabado" || "sabado"){
+    else if(dia == "sabado"){
         console.log("Vamos a la playa")
         alert("Vamos a la playa")
     }
-    else if(dia = "Domingo" || "domingo"){
+    else if(dia == "domingo"){
         console.log("O no, mañana es lunes :o")
         alert("O no, mañana es lunes :o")
     }
