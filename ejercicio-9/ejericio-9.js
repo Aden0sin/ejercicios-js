55657bc8f4b5ba8c99871e111dadf8f1e0cbcd91
Rodolfo Santini <itsmrsantini@gmail.com>
2024-02-04 14:57:58 -0600

    nuevo esqueleto, probando si sirve

--- ejercicio-9/ejericio-9.js
+++ ejercicio-9/ejericio-9.js
@@ -5,6 +5,7 @@ El topping de KitKat cuesta 15 MXN.
 El topping de brownie cuesta 20 MXN.
 */
 
+/*
 function ordenDeHelado(){
 alert("Bienvenido a Eskimo!")
 let usuario = prompt("Brindanos tu nombre por favor");
@@ -71,12 +72,14 @@ alert( usuario + " el total de su orden es de " + total);
 
 ordenDeHelado()
 
+Probando otro metodo, dejando esto de comentario de momento
+*/
 
 
 
-let inventario = [{ name: "helado", precio: 50, disponible: si },
-                 { name: "kitkat", precio: 15, disponible: no },
-                 { name: "brownie", precio: 20, disponible: si},
+let inventario = [{ name: "helado", precio: 50, disponible: "si" },
+                 { name: "kitkat", precio: 15, disponible: "no" },
+                 { name: "brownie", precio: 20, disponible: "si"},
 ]
 
 function disponibilidad(producto){ 
@@ -84,10 +87,10 @@ function disponibilidad(producto){
 
     if (prod) {
         if (prod.disponible  === "si"){
-
+        
         }
         else if{
-            alert("Lo sentimos, este articulo no esta disponible");
+            alert("Lo sentimos, este articulo no esta disponible, por favor vuelve a hacer tu orden");
             return;
         }
 
@@ -101,7 +104,38 @@ function disponibilidad(producto){
     }
     } 
 
-
-
-
 }
+
+function obtenerPrecio(producto){
+    let prod = inventario.find((element) => element.name === producto);
+    if(prod){
+        return prod.precio;}
+    else{
+    alert("error, precio no encontrado!");
+    }
+}
+
+function ordenarHelado2(){
+    alert("Bienvenido a Eskimo!");
+    let usuario = prompt("Brindanos tu nombre por favor");
+let helado = prompt("Deseas un helado? Responde Si o No"); 
+    disponibilidad(helado);
+    let precioHelado = obtenerPrecio(helado);
+    
+    let kitkat = prompt("Deseas añadir topping de kitkat? Responde Si o No"); 
+    disponibilidad(kitkat);
+    let precioKitkat = obtenerPrecio(kitkat);
+
+    let bronie = prompt("Deseas añadir topping de brownie? Responde Si o No"); 
+    disponibilidad(brownie);
+    let precioBrownie = obtenerPrecio(brownie);
+
+    let total = precioHelado + precioKitkat + precioBrownie;
+    alert(usuario + ", el total de su orden es " + total)
+
+    }
+
+    ordenarHelado2();
+
+
+
